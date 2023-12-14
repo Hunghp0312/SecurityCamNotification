3b95f37f48dc897db443252b1fa9241fb103fe1b
hunghp0312 <hoanghung31200@gmail.com>
2023-12-14 21:42:44 +0700

    First commit

---
+++ app/src/main/java/com/hunghp312/securitynotification/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hunghp312.securitynotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
